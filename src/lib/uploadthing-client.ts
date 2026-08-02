04d6dd3f11f5591eb2d884f1b9eb4e6ccc77e9a9
Leonidas <284042396+LeonidasSpart@users.noreply.github.com>
2026-08-02 13:04:03 +0000

    Remove uploadthing-client.ts again

--- src/lib/uploadthing-client.ts
+++
@@ -1,4 +0,0 @@
-import { generateReactHelpers } from "@uploadthing/react";
-import { OurFileRouter } from "@/lib/uploadthing";
-
-export const { useUploadThing, uploadFiles } = generateReactHelpers<OurFileRouter>();
