77b9e3f7512cd49069751aa061ad5c0c309dd86c
Leonidas <284042396+LeonidasSpart@users.noreply.github.com>
2026-08-05 00:57:59 +0200

    Add PricingPage component with PricingCards

---
+++ src/app/pricing/page.tsx
@@ -0,0 +1,9 @@
+import PricingCards from "@/components/PricingCards";
+
+export default function PricingPage() {
+  return (
+    <div className="max-w-6xl mx-auto py-12 px-4">
+      <PricingCards />
+    </div>
+  );
+}
