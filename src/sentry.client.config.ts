2469451d05085119664cf606734aae3d02ba1a23
Leonidas <284042396+LeonidasSpart@users.noreply.github.com>
2026-08-04 13:54:35 +0200

    Refactor Sentry integration to use replayIntegration

--- src/sentry.client.config.ts
+++ src/sentry.client.config.ts
@@ -1,4 +1,5 @@
 import * as Sentry from "@sentry/nextjs";
+import { replayIntegration } from "@sentry/nextjs";
 
 Sentry.init({
   dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
@@ -6,5 +7,10 @@ Sentry.init({
   tracesSampleRate: 0.2,
   replaysSessionSampleRate: 0.1,
   replaysOnErrorSampleRate: 1.0,
-  integrations: [new Sentry.Replay({ maskAllText: false, blockAllMedia: false })],
+  integrations: [
+    replayIntegration({
+      maskAllText: false,
+      blockAllMedia: false,
+    }),
+  ],
 });
